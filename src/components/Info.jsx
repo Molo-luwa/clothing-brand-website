25563c88b3dd33c8fb2b090ba10c2d2a1460bcd7
mololuwa <tomisola2008@gmail.com>
2026-02-18 23:59:24 +0100

    added info

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import { p } from 'framer-motion/m';
+import React from 'react';
+
+const Footerr = () => {
+  return (
+    <p>
+All orders are processed within 5-7 business days before they are sent out for delivery. </p>
+    
+  );
+};
+
+export default Info;
