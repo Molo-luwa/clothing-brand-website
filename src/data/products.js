a725622bb288d6ed2a74af0fd8882d79179c7c29
mololuwa <tomisola2008@gmail.com>
2026-02-25 18:46:34 +0100

    changed simgle image to array of image to activate scroller

--- src/data/products.js
+++ src/data/products.js
@@ -1,12 +1,127 @@
 export const products = [
-  { id: 'antho-prevails-white', name: "ANTHO PREVAILS TEE", color: "WHITE", price: "15,000", img: "/images/gridimg/white_prevails.png", isComingSoon: false },
-  { id: 'antho-prevails-black', name: "ANTHO PREVAILS TEE", color: "BLACK", price: "15,000", img: "/images/gridimg/black_prevails.png", isComingSoon: false },
-  { id: 'antho-global-white', name: "ANTHO GLOBAL TEE", color: "WHITE", price: "30,000", img: "/images/gridimg/global_tee.png", isComingSoon: false },
-  { id: 'antho-global-black', name: "ANTHO GLOBAL TEE", color: "BLACK", price: "30,000", img: "/images/gridimg/global_tee_black.png", isComingSoon: false },
-  { id: 'antho-babies-white', name: "ANTHO BABIES", color: "WHITE", price: "25,000", img: "/images/gridimg/white_glp.png", isComingSoon: false },
-  { id: 'antho-babies-black', name: "ANTHO BABIES", color: "BLACK", price: "25,000", img: "/images/gridimg/black_glp.png", isComingSoon: false },
-  { id: 'antho-npng-grey', name: "ANTHO “NPNG” SWEATS", color: "GREY", price: "25,000", img: "/logo.png", isComingSoon: true },
-  { id: 'antho-npng-black', name: "ANTHO “NPNG” SWEATS", color: "BLACK", price: "25,000", img: "/logo.png", isComingSoon: true },
-  { id: 'antho-99-polo', name: "ANTHO 99' POLO", color: "WHITE", price: "45,000", img: "/logo.png", isComingSoon: true },
-  { id: 'antho-family-pack', name: "ANTHO FAMILY PACK", color: "PACK", price: "125,000", img: "/logo.png", isComingSoon: true },
+  { 
+    id: 'antho-prevails-white', 
+    name: "ANTHO PREVAILS TEE", 
+    color: "WHITE", 
+    price: "15,000", 
+    img: "/images/gridimg/white_prevails.png",
+    images: [
+      "/images/gridimg/white_prevails.png",
+      "/images/white_prevails/IMG_6714.JPEG",
+      "/images/white_prevails/IMG_6590.JPEG",
+      "/images/white_prevails/IMG_6630.JPEG",
+      "/images/white_prevails/IMG_6598.JPEG"
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-prevails-black', 
+    name: "ANTHO PREVAILS TEE", 
+    color: "BLACK", 
+    price: "15,000", 
+    img: "/images/gridimg/black_prevails.png",
+    images: [
+      "/images/gridimg/black_prevails.png",
+      "/images/black_prevails/IMG_4911.PNG",
+      "/images/black_prevails/IMG_6738.JPEG",
+      "/images/black_prevails/IMG_6594.JPEG",
+      "/images/black_prevails/IMG_6704.JPEG",
+      "/images/black_prevails/IMG_6715.JPEG"
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-global-white', 
+    name: "ANTHO GLOBAL TEE", 
+    color: "WHITE", 
+    price: "30,000", 
+    img: "/images/gridimg/global_tee.png",
+    images: [
+      "/images/gridimg/global_tee.png",
+      "/images/global_white/IMG_6580.JPEG",
+      "/images/global_white/IMG_6733.JPEG",
+      "/images/global_white/IMG_6710.JPEG",
+      "/images/global_white/IMG_6727.JPEG",
+      "/images/global_white/IMG_4344.jpg" 
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-global-black', 
+    name: "ANTHO GLOBAL TEE", 
+    color: "BLACK", 
+    price: "30,000", 
+    img: "/images/gridimg/global_tee_black.png",
+    images: [
+      "/images/gridimg/global_tee_black.png",
+      "/images/global_black/IMG_6729.JPEG",
+      "/images/global_black/IMG_6550.jpg",
+      "/images/global_black/IMG_6693.JPEG",
+      "/images/global_black/IMG_4346.jpg" 
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-babies-white', 
+    name: "ANTHO BABIES", 
+    color: "WHITE", 
+    price: "25,000", 
+    img: "/images/gridimg/white_glp.png",
+    images: [
+      "/images/gridimg/white_glp.png",
+      "/images/bby_white/IMG_6721.JPEG",
+      "/images/bby_white/IMG_6625.JPEG",
+      "/images/bby_white/IMG_6629.JPEG",
+      "/images/bby_white/IMG_6585.JPEG",
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-babies-black', 
+    name: "ANTHO BABIES", 
+    color: "BLACK", 
+    price: "25,000", 
+    img: "/images/gridimg/black_glp.png",
+    images: [
+      "/images/gridimg/black_glp.png",
+      "/images/bby_blck/IMG_6694.JPEG",
+      "/images/bby_blck/IMG_6730.JPEG",
+      "/images/bby_blck/IMG_6586.JPEG",
+      "/images/bby_blck/IMG_6698.JPEG",
+    ],
+    isComingSoon: false 
+  },
+  { 
+    id: 'antho-npng-grey', 
+    name: "ANTHO “NPNG” SWEATS", 
+    color: "GREY", 
+    price: "25,000", 
+    img: "/logo.png",
+    // No images array (coming soon)
+    isComingSoon: true 
+  },
+  { 
+    id: 'antho-npng-black', 
+    name: "ANTHO “NPNG” SWEATS", 
+    color: "BLACK", 
+    price: "25,000", 
+    img: "/logo.png",
+    isComingSoon: true 
+  },
+  { 
+    id: 'antho-99-polo', 
+    name: "ANTHO 99' POLO", 
+    color: "WHITE", 
+    price: "45,000", 
+    img: "/logo.png",
+    isComingSoon: true 
+  },
+  { 
+    id: 'antho-family-pack', 
+    name: "ANTHO FAMILY PACK", 
+    color: "PACK", 
+    price: "125,000", 
+    img: "/logo.png",
+    isComingSoon: true 
+  },
 ];
